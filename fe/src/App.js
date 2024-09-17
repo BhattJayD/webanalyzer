ad4639b1c8850ac48067b4ab9dd5fd2323f69d10
bhattjayd <cbhatt526@gmail.com>
2024-09-17 10:17:40 +0530

    url validation

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css"; // For styling
+import { urlRegex } from "./utils/helper";
 
 const App = () => {
+  const [url, setUrl] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Prevents page reload
+    if (!url.match(urlRegex)) {
+      console.log("not valid url");
+      return;
+    }
+    console.log(url); // Logs the input value
+  };
+
   return (
     <div className="chat-container">
-      <div className="input-box">
+      <form className="input-box" onSubmit={handleSubmit}>
         <input
           type="text"
+          onChange={(e) => setUrl(e.target.value)}
+          value={url} // Controlled input to manage its value in state
           placeholder="Search for vulnerability on your site"
         />
-        <button className="send-btn">&#9654;</button>
-      </div>
+        <button type="submit" className="send-btn">
+          &#9654;
+        </button>
+      </form>
     </div>
   );
 };
---
+++ fe/src/utils/helper.js
@@ -0,0 +1,3 @@
+// eslint-disable-next-line
+export const urlRegex =
+  "^(https?://)?([da-z.-]+).([a-z.]{2,6})(:[0-9]+)?(/[^s]*)?$";
